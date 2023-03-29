2202f852e0041ee116a374f34cfe01b288952f5b
mmindd <supaphit.mind@gmail.com>
2023-03-29 14:21:23 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+
+     static void Main(string[] args){
+        Console.WriteLine("input peoples");
+        int peoples = int.Parse(Console.ReadLine());
+
+
+        if(peoples > 1){
+            if (peoples <= 10000){
+                Console.WriteLine(" ");
+            }
+        }
+
+
+    }
+
+}
